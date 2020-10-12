3e1856b4021a8f1abd8c2f485921a460d69df3e3
Mladen <mladen.lalovic@gmail.com>
2020-10-12 13:06:23 +0200

    modified endless recyclerview scrolling for RepoAdapter.kt

--- app/src/main/java/com/example/githatch/presentation/repo/RepoAdapter.kt
+++ app/src/main/java/com/example/githatch/presentation/repo/RepoAdapter.kt
@@ -52,7 +52,7 @@ class RepoAdapter(recyclerView: RecyclerView, private var isRepoActivity: Boolea
                     Log.i("MYTAG", "lastvisibleitem position: $lastVisibleItem ")
                     Log.i("MYTAG", "repoList size: ${repoList.size} ")
 
-                    if (!loading && totalItemCount - 1 <= lastVisibleItem + 25  && lastVisibleItem > repoList.size - 5) {
+                    if (!loading && totalItemCount - 1 <= lastVisibleItem + 20  && lastVisibleItem > repoList.size - 20) {
                         onLoadMoreListener.onLoadMore()
                         loading = true
                     }
